a4caaafb3075b1ec43b3d396ead8838dbe7157d1
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-10-17 15:24:41 +0700

    Membuat kolom pertama didalam row

--- app/src/main/java/com/example/p3_appmob/Tataletak.kt
+++ app/src/main/java/com/example/p3_appmob/Tataletak.kt
@@ -52,6 +52,11 @@ fun TataletakBox(modifier: Modifier){
 @Composable
 fun TataletakRowColumn(modifier: Modifier){
     Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-
+        //Kolom1
+        Column() {
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen3Kolom1")
+        }
     }
 }
