7a270fadc5ddd48de9d75a7299f8bb726f08b08c
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-10-17 15:06:32 +0700

    Menambahkan Column dan modifiersnya

--- app/src/main/java/com/example/p3_appmob/Tataletak.kt
+++ app/src/main/java/com/example/p3_appmob/Tataletak.kt
@@ -1,9 +1,14 @@
 package com.example.p3_appmob
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Tataletak(modifier: Modifier) {
+    Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
 
+    }
 }
