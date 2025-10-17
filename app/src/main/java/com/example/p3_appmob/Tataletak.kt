6e1ca77178336bd8ee659a3100fc8aecffb7e20d
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2025-10-17 15:07:29 +0700

    Menambahkan text dalam kolom

--- app/src/main/java/com/example/p3_appmob/Tataletak.kt
+++ app/src/main/java/com/example/p3_appmob/Tataletak.kt
@@ -2,6 +2,7 @@ package com.example.p3_appmob
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -9,6 +10,6 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun Tataletak(modifier: Modifier) {
     Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
-
+        Text(text = "Komponen1")
     }
 }
